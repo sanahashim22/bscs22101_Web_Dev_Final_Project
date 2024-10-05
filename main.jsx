9539b475521cff28c4064aa1734d6e6f853208e4
sanahashim22 <123752530+sanahashim22@users.noreply.github.com>
2024-10-05 20:55:29 +0500

    Update logo and title also add link for mobile responsiveness

---
+++ main.jsx
@@ -0,0 +1,15 @@
+
+// http://localhost:5173/ run in chrome
+
+
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+//import './index.css'
+import './css/App.css'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
